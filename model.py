1af79267c7a11f5074bbff9d148d2a37d463457b
GodWriter <18917565247@163.com>
2021-03-19 20:29:32 +0800

    commit.

---
+++ model.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
